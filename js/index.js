ab3f8f0c1ec6e9b02d5e80e3618a5690ebbf02d0
raulmoreno17 <66703343+raulmoreno17@users.noreply.github.com>
2020-07-10 22:36:03 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -19,11 +19,11 @@ function roll()
   if (randomNumber1 == randomNumber2) {
     document.querySelector(".winner").textContent = "Empate!⭕";
   } else if (randomNumber1 < randomNumber2) {
-    document.querySelector(".winner").textContent = "👑 Jugador 2 gana!";
+    document.querySelector(".winner").textContent = "Jugador 2 gana! 👑 ";
     document.querySelector("img.img2").classList.add("big");
     document.querySelector("img.img1").classList.add("small");
   } else if (randomNumber1 > randomNumber2) {
-    document.querySelector(".winner").textContent = "Jugador 1 gana! 👑";
+    document.querySelector(".winner").textContent = "👑 Jugador 1 gana!";
     document.querySelector("img.img1").classList.add("big");
     document.querySelector("img.img2").classList.add("small");
   }
